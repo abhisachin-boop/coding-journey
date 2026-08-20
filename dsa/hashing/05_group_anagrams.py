e621baaf0ab14fd2968659da6bdac900e54e3cbd
Sachin Babu <abhisachin024@gmail.com>
2026-08-20 13:30:05 +0530

    feat: add group anagrams hashing problem

---
+++ dsa/hashing/05_group_anagrams.py
@@ -0,0 +1,14 @@
+# Group words that are anagrams using a hash map
+
+def group_anagrams(words):
+    groups = {}
+
+    for word in words:
+        key = "".join(sorted(word))
+        groups.setdefault(key, []).append(word)
+
+    return list(groups.values())
+
+
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(group_anagrams(words))
